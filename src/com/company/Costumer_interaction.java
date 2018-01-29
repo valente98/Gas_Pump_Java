f62e9608444f106caee38cf0179acdb6a9706abc
valente alvarez <valvarez@basecampcodingacademy.org>
2018-01-29 10:01:34 -0600

    added more quote for the user. then call them on the main.

--- src/com/company/Costumer_interaction.java
+++ src/com/company/Costumer_interaction.java
@@ -2,6 +2,8 @@ package com.company;
 
 public class Costumer_interaction {
     public void Welcome() {
-        System.out.println("Welcome!");
+        System.out.println("Welcome to Shell!\n");
+        System.out.println("Would you like");
+        System.out.println("\t[1] Prepay \n \t[2] Pay After");
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner input = new Scanner(System.in);
+        Costumer_interaction costumer =  new Costumer_interaction();
+        costumer.Welcome();
+        String payment_choice = input.nextLine();
     }
 }
