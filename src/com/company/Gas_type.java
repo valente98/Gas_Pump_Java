11f1fa194fb686422ff537b464b2c1211dd6eb95
valente alvarez <valvarez@basecampcodingacademy.org>
2018-01-29 11:19:00 -0600

    created an object for the gas pump. on the main started an if statement

--- src/com/company/Gas_type.java
+++ src/com/company/Gas_type.java
@@ -1,7 +1,18 @@
 package com.company;
 
 public class Gas_type {
-    public int regular(){
+    public double gas_amount;
+    public double gallon_amount;
+    public String gas_type;
+
+    Gas_type(double gallon_amount, double gas_amount, String gas_type){
+        this.gallon_amount = gallon_amount;
+        this.gas_amount = gas_amount;
+        this.gas_type = gas_type;
+    }
+
+    public double regular(){
         return 0;
     }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,5 +11,26 @@ public class Main {
         String GasType = input.nextLine();
         costumer.PrepayorPayafter();
         String PrepayorAfter = input.nextLine();
+        if(GasType.equals("1") || GasType.equals("2") || GasType.equals("3")){
+            if(PrepayorAfter.equals("1")){
+                System.out.println("How much would you like to pay?");
+                String price = input.nextLine();
+                double amount= Double.parseDouble(price);
+                Gas_type gas = new Gas_type(0, amount, GasType);
+                System.out.println("Your total gallons is " + gas);
+                System.out.println("Thank you for fueling with Shell. \nHave a great day!");
+            }else if(PrepayorAfter.equals("2")){
+                System.out.println("How many gallons would you like?");
+                String gallons = input.nextLine();
+                double amount= Double.parseDouble(gallons);
+                Gas_type gas = new Gas_type(amount, 0, GasType);
+                System.out.println("Your total price is " + gas);
+                System.out.println("Thank you for fueling with Shell. \nHave a great day!");
+            }else {
+                System.out.println("Sorry invalid choice.");
+            }
+        }else{
+           System.out.println("Sorry Invalid choice");
+        }
     }
 }
